3028e8818637e12f50f6b1040b03c51dd78d55f1
KEENTO <handball0321@gmail.com>
2022-04-05 13:54:45 -0700

    Add youtube player API on game page

--- js/index.js
+++ js/index.js
@@ -394,3 +394,59 @@ swipeRightBtn.addEventListener("click", () => keyRight(lengthOfSquare));
 swipeLeftBtn.addEventListener("click", () => keyLeft(lengthOfSquare));
 swipeUpBtn.addEventListener("click", () => keyUp(lengthOfSquare));
 swipeDownBtn.addEventListener("click", () => keyDown(lengthOfSquare));
+
+//
+//
+// Youtube Player API
+//
+//
+
+// const stopBtn = document.querySelector("#stop");
+// const pauseBtn = document.querySelector("#pause");
+// const playBtn = document.querySelector("#play");
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "200",
+    width: "400",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+      onClick: onPlayerReady,
+    },
+    playerVars: {
+      loop: 1,
+      rel: 0,
+    },
+    videoId: "G4TUmkg4ojg",
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  //   event.target.mute();
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {}
+function stopVideo() {
+  player.stopVideo();
+}
+
+// pauseBtn.addEventListener("click", () => player.pauseVideo());
+startNewGameBtn.addEventListener("click", () => player.playVideo());
+resetGameBtn.addEventListener("click", () => player.stopVideo());
